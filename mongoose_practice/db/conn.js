2a25b6e413bc822cfed51804733cc16d2729fefa
Michle99 <sammiyonyx@gmail.com>
2023-11-23 14:19:40 -0500

    update conn.js and restaurants.js

--- mongoose_practice/db/conn.js
+++ mongoose_practice/db/conn.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 import '../loadEnv.js'
 
-// const connectionString = "mongodb+srv://nice:1n7prcx5gQHt6IcS@mongopt.auarr0p.mongodb.net/?retryWrites=true&w=majority";
-// console.log("Connection string:", connectionString);
+const connectionString = process.env.ATLAS_URI;
+console.log("Connection string:", connectionString);
 
 const connectToDatabase = async () => {
     try {
-      await mongoose.connect("mongodb+srv://nice:1n7prcx5gQHt6IcS@mongopt.auarr0p.mongodb.net",
+      await mongoose.connect(connectionString,
       /**
        * Gotta add "dbName" to specifically connect to the specific collection
        * or else it wont connect to the collections u want in MongoDB 
